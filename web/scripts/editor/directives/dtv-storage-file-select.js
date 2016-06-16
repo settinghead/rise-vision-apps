bca7d4382f10be99e30a96640b79da9d856a54a6
rodrigopavezi <rodrigopavezi@gmail.com>
2016-06-16 11:31:01 -0400

    File Dialog

---
+++ web/scripts/editor/directives/dtv-storage-file-select.js
@@ -0,0 +1,15 @@
+(function () {
+
+  'use strict';
+
+  angular.module('risevision.editor.directives')
+    .directive('fileSelect', [function () {
+      return {
+        link: function (scope, element, attributes) {
+          element.bind('change', function () {
+            console.log(this.files.length);
+          });
+        }
+      };
+    }]);
+})();
