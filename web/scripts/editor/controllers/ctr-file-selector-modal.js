ab1ce07a786cc06e61f6d0593b6f29a885ab988f
Alex Deaconu <alex.deaconu@gmail.com>
2016-06-16 11:24:48 -0400

    file selector modal with url

--- web/scripts/editor/controllers/ctr-file-selector-modal.js
+++ web/scripts/editor/controllers/ctr-file-selector-modal.js
@@ -1,23 +1,11 @@
 'use strict';
 
 angular.module('risevision.editor.controllers')
-  .controller('FileSelectorModal', ['$scope', 'editorFactory',
-    'presentationParser', 'distributionParser',
-    function ($scope, editorFactory, presentationParser, distributionParser) {
-      $scope.factory = editorFactory;
-      presentationParser.updatePresentation(editorFactory.presentation);
-      distributionParser.updateDistribution(editorFactory.presentation);
-
-      $scope.codemirrorOptions = {
-        lineNumbers: true,
-        theme: 'twilight',
-        lineWrapping: false,
-        mode: 'htmlmixed'
+  .controller('FileSelectorModal', ['$scope', '$modalInstance',
+    function ($scope, $modalInstance) {
+      $scope.dismiss = function () {
+        $modalInstance.dismiss();
       };
 
-      $scope.$on('$destroy', function () {
-        presentationParser.parsePresentation(editorFactory.presentation);
-        distributionParser.parseDistribution(editorFactory.presentation);
-      });
     }
   ]); //ctr
