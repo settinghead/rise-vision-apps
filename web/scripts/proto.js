abb1ee41f0accdfd4ec49b48c41299de3fe2ed4c
RV-Adrian <adrian.dalimonte@risevision.com>
2016-07-20 12:32:59 -0400

    Staged revisions [stage-2]

--- web/scripts/proto.js
+++ web/scripts/proto.js
@@ -1,44 +1,58 @@
-
-
 $(document).on("click", "body", function() {
+
+            $('input:checkbox').click(function() {
+                event.stopPropagation();
+            });
+
+            $('.singleitem input:checkbox').change(function() {
+                if ($(this).is(":checked")) {
+                    $(this).prev().addClass("selectedBorder", 100, "linear");
+                } else {
+                    $(this).prev().removeClass("selectedBorder", 100, "linear");
+                    $('input:checkbox.soloselect').not(this).removeClass("selectedBorder", 100, "linear");  
+                }
+            });
+
+            $('.folder input:checkbox').change(function() {
+                if ($(this).is(":checked")) {
+                    $(this).closest("li").addClass("folder-extended", 100, "linear");
+                    $(this).next().next().removeClass("hidden");
+                    $(this).closest("li").addClass("selectedBorder", 100, "linear");
+                } else {
+                    $(this).closest("li").removeClass("folder-extended", 100, "linear");
+                    $(this).next().next().addClass("hidden");
+                    $(this).closest("li").removeClass("selectedBorder", 100, "linear");
+                }
+            });
+
+            $('input:checkbox.soloselect').change(function() {
+                $('input:checkbox.soloselect').not(this).prop('checked', false);  
+
+            });
+
+
+
+    //  $('input:radio').click(function() {
+    //     event.stopPropagation();
+    // });
+
+    // $('input:radio').change(function() {
+
+       
+    // });
 
    
 
-    $('input:checkbox').click(function() {
-        event.stopPropagation();
-    });
+    //  $('.folder input:checkbox').change(function() {
+    //     if ($(this).is(":checked")) {
+    //         $(this).prev().removeClass("selectedBorder");
+    //         $(this).prev().prev().addClass("selectedBorder", 100, "linear");
+    //     } else {
+    //         $(this).prev().removeClass("selectedBorder",);
+    //         $(this).prev().prev().removeClass("selectedBorder", 100, "linear");
+    //     }
+    // });
 
-    $('input:checkbox').change(function() {
-        if ($(this).is(":checked")) {
-            $(this).prev().addClass("selectedBorder", 100, "linear");
-        } else {
-            $(this).prev().removeClass("selectedBorder", 100, "linear");
-        }
-    });
-
-    $('input:radio').click(function() {
-        event.stopPropagation();
-    });
-
-    $('input:radio').change(function() {
-
-        $('input:radio').prev().removeClass("selectedBorder", 100, "linear");
-        if ($(this).is(":checked")) {
-            $(this).prev().addClass("selectedBorder", 100, "linear");
-        } else {
-            $(this).prev().removeClass("selectedBorder", 100, "linear");
-        }
-    });
-
-    $('.folder input:checkbox').change(function() {
-        if ($(this).is(":checked")) {
-            $(this).closest("li").addClass("folder-extended", 100, "linear");
-            $(this).next().next().removeClass("hidden");
-        } else {
-            $(this).closest("li").removeClass("folder-extended", 100, "linear");
-            $(this).next().next().addClass("hidden");
-        }
-    });
      $('.folder input:radio').click(function() {
 
         $('.folder input:radio').closest("li").removeClass("folder-extended");
